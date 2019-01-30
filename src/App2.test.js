01610c572670118c3a99910dfcd04ad7046458f0
Andrzej <andrzej.wower@kingfisher.com>
2019-01-30 14:32:11 +0000

    add one more test

--- src/App2.test.js
+++ src/App2.test.js
@@ -85,3 +85,9 @@ it('renders without crashing', () => {
   wait(delayTests)
   ReactDOM.unmountComponentAtNode(div);
 });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  wait(delayTests)
+  ReactDOM.unmountComponentAtNode(div);
+});
