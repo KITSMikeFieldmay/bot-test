c54bd1ddfafd7d1fa7c89e21b77fa3ae9536b8b1
Andrzej <andrzej.wower@kingfisher.com>
2019-01-30 14:50:57 +0000

    add test change

--- src/App.test.js
+++ src/App.test.js
@@ -11,7 +11,7 @@ function wait(ms){
   }
 }
 
-it('renders without crashing', () => {
+it('renders change without crashing', () => {
   const i = document.createElement('i');
   ReactDOM.render(<App />, i);
   ReactDOM.unmountComponentAtNode(i);
