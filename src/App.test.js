cf3965666eca8aa92e4f30255b44cc61806bb4ed
Andrzej <andrzej.wower@kingfisher.com>
2019-01-30 11:17:34 +0000

    add fake tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {delayTests} from "./consts";
 
 function wait(ms){
   var start = new Date().getTime();
@@ -20,62 +21,67 @@ it('renders without crashing', () => {
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  wait(6000)
+  wait(delayTests)
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  wait(6000)
+  wait(delayTests)
   ReactDOM.unmountComponentAtNode(div);
 });
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  wait(6000)
+  wait(delayTests)
   ReactDOM.unmountComponentAtNode(div);
 });
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  wait(6000)
+  wait(delayTests)
   ReactDOM.unmountComponentAtNode(div);
 });
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  wait(6000)
+  wait(delayTests)
   ReactDOM.unmountComponentAtNode(div);
 });
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  wait(6000)
+  wait(delayTests)
   ReactDOM.unmountComponentAtNode(div);
 });
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  wait(6000)
+  wait(delayTests)
   ReactDOM.unmountComponentAtNode(div);
 });
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  wait(6000)
+  wait(delayTests)
   ReactDOM.unmountComponentAtNode(div);
 });
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  wait(6000)
+  wait(delayTests)
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  wait(delayTests)
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  wait(delayTests)
   ReactDOM.unmountComponentAtNode(div);
 });
