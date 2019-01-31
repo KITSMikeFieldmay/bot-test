753ca0e116b3138b22a86b25d2e59113381aa71f
Andrzej <andrzej.wower@kingfisher.com>
2019-01-31 09:51:03 +0000

    remove test without delay

--- src/App2.test.js
+++ src/App2.test.js
@@ -14,13 +14,6 @@ function wait(ms){
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
   wait(delayTests)
   ReactDOM.unmountComponentAtNode(div);
 });
